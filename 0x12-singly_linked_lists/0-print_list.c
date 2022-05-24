0b366421657b4316e096f921de79945dc7a387e9
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-05-24 16:38:39 +0000

    corrections for 0.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - prints all elements of a list_t list
  * @h: singly linked list to print
- * Return: number of nodes.
+ * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h; i++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
-		h = h->current;
+		h = h->next;
 	}
 	return (i);
 }
