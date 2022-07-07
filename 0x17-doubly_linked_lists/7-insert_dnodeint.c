b1d83d713a281995821416515af9a3d97e0749ee
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-07-07 14:15:49 +0000

    insert error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,68 +1,53 @@
 #include "lists.h"
 
 /**
- *insert_dnodeint_at_index - Inserts a new node at a given position.
- *@h:Pointer to a pointer that points to the head node.
- *@n: Data of the pointer to be inserted at index n.
- *@idx:Index of the list where the new node should be added.
- *Return: address of the new node.
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ * insert_dnodeint_at_index - inserts a new node at given index in the list
+ * @head: pointer to head of the list
+ * @idx: index to add at, starting from 0
+ * @n: value of new node
+ * Return: new node or null
+ **/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	unsigned int len_of_d = sizeof_dlist(*h);
-	dlistint_t *temp = *h;
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	unsigned int count;
+	dlistint_t *tmp, *new, *tmp_prev;
 
-	if (idx > len_of_d + 1)
+	if (head == NULL && idx > 0)
+	return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	if (newNode == NULL)
-	{
-		return (NULL);
-	}
-	newNode->n = n;
-	if (*h == NULL)
-	{
-		newNode->next = NULL;
-		newNode->prev = NULL;
-		*h = newNode;
-	}
+	new->n = n, new->prev = new->next = NULL;
+
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
-	}
-	else
+		if (*head)
 	{
-		for (i = 0; i < (idx - 1); i++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		newNode->next = temp->next;
-		newNode->prev = temp;
-		temp->next = newNode;
+		new->next = *head;
+		(*head)->prev = new, *head = new;
 	}
-	return (newNode);
-}
-
-/**
- *sizeof_dlist - Finds the size of a doubly linked list.
- *@head: Pointer to the head of a doubly linked list.
- *Return: Size of the doubly linked list.
- */
-
-int sizeof_dlist(dlistint_t *head)
-{
-	dlistint_t *temp = head;
-	int i = 0;
-
-	while (temp != NULL)
+		else
+			*head = new;
+		return (new);
+	}
+	count = 1, tmp = (*head)->next;
+	while (tmp)
 	{
-		i++;
-		temp = temp->next;
+		if (idx == count)
+		{
+			tmp->prev->next = new, new->prev = tmp->prev;
+			new->next = tmp, tmp->prev = new;
+			return (new);
+		}
+		count++;
+		tmp_prev = tmp;
+		tmp = tmp->next;
 	}
-	return (i - 1);
+	if (tmp == NULL && count == idx)
+	{
+		tmp_prev->next = new, new->prev = tmp_prev;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
